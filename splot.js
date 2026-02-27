8830c91ae3b5f593b4f86aff44e5475e5e48237a
shafa171 <shafa.siddiqua171@outlook.com>
2026-02-26 23:53:40 -0500

    Update splot.js

--- splot.js
+++ splot.js
@@ -1,90 +1,75 @@
-//define the svg area
-var margin = {top: 30, right: 10, bottom: 10, left: 10},
-    width = 1200 - margin.left - margin.right,
-    height = 550 - margin.top - margin.bottom;
+// splot.js (D3 v3) - Scatter Plot for Task 1
+// Exposes: window.renderScatter(data, svgSelector, xKey, yKey)
 
-//define the the axes of the scatter plot
-var x = d3.scale.linear().range([50, width]),
-    y = d3.scale.linear().range([height-20,0]);
+window.renderScatter = function(data, svgSelector, xKey, yKey) {
+  var margin = {top: 30, right: 10, bottom: 45, left: 55},
+      outerW = 520,
+      outerH = 450,
+      width  = outerW - margin.left - margin.right,
+      height = outerH - margin.top - margin.bottom;
 
+  // Clear target svg
+  d3.select(svgSelector).selectAll("*").remove();
 
-
-//define the array for multi-dimensional data
-var cars = []; 
-
-//define the array to hold all lines
-//var  dots = [];
-
-//create the svg
-var svg = d3.select(".chart")
-    .attr("width", width + margin.left + margin.right)
-    .attr("height", height + margin.top + margin.bottom)
+  var svg = d3.select(svgSelector)
+    .attr("width", outerW)
+    .attr("height", outerH)
     .append("g")
     .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-//read the data from a file
-d3.csv("cars.csv", type, function(error, data) {
-    cars = data;//assign the data to the array
-    drawSPlot();//draw the graph
-});
+  // Coerce numeric (keeps consistent behavior)
+  data.forEach(function(d) {
+    d[xKey] = +d[xKey];
+    d[yKey] = +d[yKey];
+  });
 
-//draw scatter plot
-function drawSPlot(){
+  // Scales
+  var x = d3.scale.linear().range([0, width]);
+  var y = d3.scale.linear().range([height, 0]);
 
-    //define axes
-    var xAxis = d3.svg.axis()
-	   .scale(x)
-	   .orient("bottom");
-    
-    var yAxis = d3.svg.axis()
-	   .scale(y)
-	   .orient("left");
-    
-    x.domain([d3.min(cars, function(d) { return d.year; }),
-			 d3.max(cars, function(d) { return d.year; })]);
-    y.domain([d3.min(cars, function(d) { return d.power; }),
-			d3.max(cars, function(d) { return d.power; })]);
-    //draw axes
-    var xPosition = height -20;
-    svg.append("g")
-	   .attr("class", "xaxis")
-	   .attr("transform", "translate(0," + xPosition + ")")
-	   .call(xAxis);
+  x.domain(d3.extent(data, function(d){ return d[xKey]; })).nice();
+  y.domain(d3.extent(data, function(d){ return d[yKey]; })).nice();
 
-    var yPosition = 50;
-    svg.append("g")
-	   .attr("class", "yaxis")
-	   .attr("transform", "translate(" + yPosition + ", 0)")
-	   .call(yAxis);
-    
-    //draw dots
-    for (var i=0; i<cars.length; i++) {
-     
-     //draw a dot
-     var dot = svg.append("g")
-	   .append("circle")
-	   .attr("class", "dot")
-	   .attr("cx", function(d) { return x(cars[i].year); })
-	   .attr("cy", function(d) { return y(cars[i].power); })
-	   .attr("idx", i)
-	   .attr("r", 3)
-    	   .style("fill", "black")
-	   .on("mouseover", function(d) {
-               d3.select(this).style("fill", "red").attr("r", 5);  })                  
-	   .on("mouseout", function(d) {
-               d3.select(this).style("fill", "black").attr("r", 3);  });                  
-     
-     //dots.push(dot);//push the dot to the array
-    }
-}
+  // Axes
+  var xAxis = d3.svg.axis().scale(x).orient("bottom");
+  var yAxis = d3.svg.axis().scale(y).orient("left");
 
+  svg.append("g")
+    .attr("class", "xaxis")
+    .attr("transform", "translate(0," + height + ")")
+    .call(xAxis);
 
-//function to coerce numerial data
-function type(d) {
-    d.economy = +d.economy; // coerce to number
-    d.displacement = +d.displacement; // coerce to number
-    d.power = +d.power; // coerce to number
-    d.weight = +d.weight; // coerce to number
-    d.year = +d.year;
-	return d;
-}
+  svg.append("g")
+    .attr("class", "yaxis")
+    .call(yAxis);
+
+  // Axis labels (rubric)
+  svg.append("text")
+    .attr("text-anchor", "middle")
+    .attr("x", width / 2)
+    .attr("y", height + 35)
+    .text(xKey);
+
+  svg.append("text")
+    .attr("text-anchor", "middle")
+    .attr("transform", "rotate(-90)")
+    .attr("x", -height / 2)
+    .attr("y", -40)
+    .text(yKey);
+
+  // Dots
+  svg.selectAll("circle")
+    .data(data)
+    .enter()
+    .append("circle")
+    .attr("class", "sp-dot")
+    .attr("cx", function(d){ return x(d[xKey]); })
+    .attr("cy", function(d){ return y(d[yKey]); })
+    .attr("r", 3)
+    .on("mouseover", function() {
+      d3.select(this).style("fill", "red").attr("r", 5).style("opacity", 1);
+    })
+    .on("mouseout", function() {
+      d3.select(this).style("fill", "black").attr("r", 3).style("opacity", 0.75);
+    });
+};
