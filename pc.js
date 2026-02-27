d94a3a66a2373a60683b4487864d1c47facb37ae
shafa171 <shafa.siddiqua171@outlook.com>
2026-02-26 23:52:39 -0500

    Update pc.js

--- pc.js
+++ pc.js
@@ -1,103 +1,90 @@
-//define the svg area
-var margin = {top: 30, right: 10, bottom: 10, left: 10},
-    width = 1200 - margin.left - margin.right,
-    height = 550 - margin.top - margin.bottom;
+// pc.js (D3 v3) - Parallel Coordinates for Task 1
+// Exposes: window.renderPC(data, svgSelector)
 
-//define the the axes in parallel coordinate
-var x = d3.scale.ordinal().rangePoints([0, width/4], 1),
-    y = {};
+window.renderPC = function(data, svgSelector) {
+  var margin = {top: 30, right: 10, bottom: 10, left: 10},
+      outerW = 520,
+      outerH = 450,
+      width  = outerW - margin.left - margin.right,
+      height = outerH - margin.top - margin.bottom;
 
-var line = d3.svg.line()
-          .x(function(d) { return d.x; })
-          .y(function(d) { return d.y; })
-          .interpolate("linear");
+  // Clear target svg
+  d3.select(svgSelector).selectAll("*").remove();
 
-var axis = d3.svg.axis().orient("left");
-
-
-//define the array for multi-dimensional data
-var cars = []; 
-
-//define the array to hold all lines
-//var polyLines = [];
-//    dots = [];
-
-//create the svg
-var svg = d3.select(".chart")
-    .attr("width", width + margin.left + margin.right)
-    .attr("height", height + margin.top + margin.bottom)
+  // Root group
+  var svg = d3.select(svgSelector)
+    .attr("width", outerW)
+    .attr("height", outerH)
     .append("g")
     .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-//read the data from a file
-d3.csv("cars.csv", type, function(error, data) {
-    cars = data;//assign the data to the array
-    drawpc(); //draw the graph
-    //drawxyplot();
-});
-
-function drawpc() {
-    
-    // Extract the list of dimensions and create a scale for each.
-    x.domain(dimensions = d3.keys(cars[0]).filter(function(d) {
-                return d != "name" && 
-                (y[d] = d3.scale.linear()
-                            .domain(d3.extent(cars, function(p) { return +p[d]; }))
-            .range([height, 0]));
-    }));
-
-    // Add polylines
-    for (var i=0; i< cars.length; i++) {
-        var lineData = [];
-        
-        //prepare data
-        for (var prop in cars[i]) {
-             if (prop != "name" ) {
-                 var point = {};
-                 var val = cars[i][prop];
-                 point['x'] = x(prop);
-                 point['y'] = y[prop](val);
-                 lineData.push(point);
-             }
-        }
-        
-        //draw a poly line
-        var pLine=svg.append("g")
-                    .attr("class", "polyline")    
-                    .append("path")
-                    .attr("d", line(lineData))
-                    .on("mouseover", function() {//highlight a line when cursor hovering
-                        d3.select(this).style("stroke", "red").style("stroke-width", 5);})                  
-                    .on("mouseout", function(d) {//restore the original color
-                        d3.select(this).style("stroke", "#666").style("stroke-width", 1);});
-                        
-//        polyLines.push(pLine); //add a polyline to the array
-
+  // Coerce numeric values where possible (keeps your original behavior)
+  data.forEach(function(d) {
+    for (var k in d) {
+      if (k !== "name") {
+        var v = +d[k];
+        if (!isNaN(v)) d[k] = v;
+      }
     }
+  });
 
-    //add dimension axes 
-    var g = svg.selectAll(".dimension")
-	   .data(dimensions)
-	   .enter().append("g")
-	   .attr("class", "dimension")
-	   .attr("transform", function(d) { return "translate(" + x(d) + ")"; });
-    
-    //add an axis and title.
-    g.append("g")
-	   .attr("class", "axis")
-	   .each(function(d) { d3.select(this).call(axis.scale(y[d])); })
-	   .append("text")
-	   .style("text-anchor", "middle")
-	   .attr("y", -9)
-	   .text(function(d) { return d; });
-    
+  // Scales
+  var x = d3.scale.ordinal().rangePoints([0, width], 1);
+  var y = {};
+
+  var line = d3.svg.line()
+    .x(function(d) { return d.x; })
+    .y(function(d) { return d.y; })
+    .interpolate("linear");
+
+  var axis = d3.svg.axis().orient("left");
+
+  // Dimensions: all numeric columns except "name"
+  var dimensions = d3.keys(data[0]).filter(function(dim) {
+    if (dim === "name") return false;
+
+    // only keep dimensions that are numeric in the data
+    var ok = data.some(function(p) { return !isNaN(+p[dim]); });
+    if (!ok) return false;
+
+    y[dim] = d3.scale.linear()
+      .domain(d3.extent(data, function(p) { return +p[dim]; }))
+      .range([height, 0]);
+
+    return true;
+  });
+
+  x.domain(dimensions);
+
+  // Draw polylines (one per row)
+  data.forEach(function(row) {
+    var lineData = dimensions.map(function(dim) {
+      return { x: x(dim), y: y[dim](+row[dim]) };
+    });
+
+    svg.append("path")
+      .attr("class", "pc-line")
+      .attr("d", line(lineData))
+      .on("mouseover", function() {
+        d3.select(this).style("stroke", "red").style("stroke-width", 3).style("opacity", 1);
+      })
+      .on("mouseout", function() {
+        d3.select(this).style("stroke", "#666").style("stroke-width", 1).style("opacity", 0.35);
+      });
+  });
+
+  // Axes
+  var g = svg.selectAll(".dimension")
+    .data(dimensions)
+    .enter().append("g")
+    .attr("class", "dimension")
+    .attr("transform", function(d) { return "translate(" + x(d) + ")"; });
+
+  g.append("g")
+    .attr("class", "axis")
+    .each(function(d) { d3.select(this).call(axis.scale(y[d])); })
+    .append("text")
+    .style("text-anchor", "middle")
+    .attr("y", -9)
+    .text(function(d) { return d; });
 };
-
-function type(d) {
-    d.economy = +d.economy; // coerce to number
-    d.displacement = +d.displacement; // coerce to number
-    d.power = +d.power; // coerce to number
-    d.weight = +d.weight; // coerce to number
-    d.year = +d.year;
-	return d;
-}
