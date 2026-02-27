fcb4cce373bc2d5ad4f2d92ab3aaa6a25aaeba33
shafa171 <shafa.siddiqua171@outlook.com>
2026-02-26 23:57:54 -0500

    Update splot.js

--- splot.js
+++ splot.js
@@ -1,75 +1,110 @@
-// splot.js (D3 v3) - Scatter Plot for Task 1
-// Exposes: window.renderScatter(data, svgSelector, xKey, yKey)
+// splot.js (D3 v3) - Scatter Plot (Coordinated + Bonus dropdown updates)
+// Exposes:
+//   window.renderScatter(data, svgSelector, xKey, yKey)
+//   window.updateScatterAxes(xKey, yKey)
+
+var sp = {}; // namespace
 
 window.renderScatter = function(data, svgSelector, xKey, yKey) {
+  sp.data = data;
+  sp.svgSelector = svgSelector;
+
   var margin = {top: 30, right: 10, bottom: 45, left: 55},
       outerW = 520,
-      outerH = 450,
-      width  = outerW - margin.left - margin.right,
-      height = outerH - margin.top - margin.bottom;
+      outerH = 450;
 
-  // Clear target svg
+  sp.margin = margin;
+  sp.outerW = outerW;
+  sp.outerH = outerH;
+  sp.width  = outerW - margin.left - margin.right;
+  sp.height = outerH - margin.top - margin.bottom;
+
+  // clear
   d3.select(svgSelector).selectAll("*").remove();
 
-  var svg = d3.select(svgSelector)
+  sp.root = d3.select(svgSelector)
     .attr("width", outerW)
     .attr("height", outerH)
     .append("g")
     .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-  // Coerce numeric (keeps consistent behavior)
-  data.forEach(function(d) {
+  // scales + axes
+  sp.x = d3.scale.linear().range([0, sp.width]);
+  sp.y = d3.scale.linear().range([sp.height, 0]);
+  sp.xAxis = d3.svg.axis().scale(sp.x).orient("bottom");
+  sp.yAxis = d3.svg.axis().scale(sp.y).orient("left");
+
+  sp.xG = sp.root.append("g")
+    .attr("class", "xaxis")
+    .attr("transform", "translate(0," + sp.height + ")");
+
+  sp.yG = sp.root.append("g")
+    .attr("class", "yaxis");
+
+  // labels
+  sp.xLabel = sp.root.append("text")
+    .attr("text-anchor", "middle")
+    .attr("x", sp.width / 2)
+    .attr("y", sp.height + 35);
+
+  sp.yLabel = sp.root.append("text")
+    .attr("text-anchor", "middle")
+    .attr("transform", "rotate(-90)")
+    .attr("x", -sp.height / 2)
+    .attr("y", -40);
+
+  // first draw
+  updateScatterInternal(xKey, yKey, true);
+};
+
+function updateScatterInternal(xKey, yKey, first) {
+  sp.xKey = xKey;
+  sp.yKey = yKey;
+
+  // coerce
+  sp.data.forEach(function(d) {
     d[xKey] = +d[xKey];
     d[yKey] = +d[yKey];
   });
 
-  // Scales
-  var x = d3.scale.linear().range([0, width]);
-  var y = d3.scale.linear().range([height, 0]);
+  sp.x.domain(d3.extent(sp.data, function(d){ return d[xKey]; })).nice();
+  sp.y.domain(d3.extent(sp.data, function(d){ return d[yKey]; })).nice();
 
-  x.domain(d3.extent(data, function(d){ return d[xKey]; })).nice();
-  y.domain(d3.extent(data, function(d){ return d[yKey]; })).nice();
+  sp.xG.call(sp.xAxis);
+  sp.yG.call(sp.yAxis);
 
-  // Axes
-  var xAxis = d3.svg.axis().scale(x).orient("bottom");
-  var yAxis = d3.svg.axis().scale(y).orient("left");
+  sp.xLabel.text(xKey);
+  sp.yLabel.text(yKey);
 
-  svg.append("g")
-    .attr("class", "xaxis")
-    .attr("transform", "translate(0," + height + ")")
-    .call(xAxis);
+  var dots = sp.root.selectAll("circle")
+    .data(sp.data, function(d){ return d.__id__; });
 
-  svg.append("g")
-    .attr("class", "yaxis")
-    .call(yAxis);
-
-  // Axis labels (rubric)
-  svg.append("text")
-    .attr("text-anchor", "middle")
-    .attr("x", width / 2)
-    .attr("y", height + 35)
-    .text(xKey);
-
-  svg.append("text")
-    .attr("text-anchor", "middle")
-    .attr("transform", "rotate(-90)")
-    .attr("x", -height / 2)
-    .attr("y", -40)
-    .text(yKey);
-
-  // Dots
-  svg.selectAll("circle")
-    .data(data)
-    .enter()
-    .append("circle")
+  // enter
+  dots.enter().append("circle")
     .attr("class", "sp-dot")
-    .attr("cx", function(d){ return x(d[xKey]); })
-    .attr("cy", function(d){ return y(d[yKey]); })
+    .attr("data-id", function(d){ return d.__id__; })
     .attr("r", 3)
-    .on("mouseover", function() {
-      d3.select(this).style("fill", "red").attr("r", 5).style("opacity", 1);
+    .on("mouseover", function(d){
+      if (window.highlightById) window.highlightById(d.__id__);
     })
-    .on("mouseout", function() {
-      d3.select(this).style("fill", "black").attr("r", 3).style("opacity", 0.75);
+    .on("mouseout", function(d){
+      if (window.unhighlightById) window.unhighlightById(d.__id__);
     });
+
+  // update positions
+  if (first) {
+    dots.attr("cx", function(d){ return sp.x(d[xKey]); })
+        .attr("cy", function(d){ return sp.y(d[yKey]); });
+  } else {
+    sp.root.selectAll("circle")
+      .transition().duration(300)
+      .attr("cx", function(d){ return sp.x(d[xKey]); })
+      .attr("cy", function(d){ return sp.y(d[yKey]); });
+  }
+
+  dots.exit().remove();
+}
+
+window.updateScatterAxes = function(xKey, yKey) {
+  updateScatterInternal(xKey, yKey, false);
 };
